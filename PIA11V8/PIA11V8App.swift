d751281bb9a73e9e8a389b139ab6bade4f1a65c7
Jayakoddi <priyanathan.christy@gmail.com>
2022-10-31 09:09:35 +0100

    Initial Commit

---
+++ PIA11V8/PIA11V8App.swift
@@ -0,0 +1,17 @@
+//
+//  PIA11V8App.swift
+//  PIA11V8
+//
+//  Created by Christy Dinakaran on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct PIA11V8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
